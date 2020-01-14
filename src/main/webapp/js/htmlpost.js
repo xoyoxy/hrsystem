3de4f653c2e294e34ec5cdaef4324185a43d0294
xoyoxy <zhoukun916@126.com>
2020-01-14 22:12:28 +0800

    1. event.preventDefault()

--- src/main/webapp/js/htmlpost.js
+++ src/main/webapp/js/htmlpost.js
@@ -1,3 +1,4 @@
+/*
 $.ajax({
     type : "post",
     url : "$!base/aaa/getList",
@@ -9,4 +10,35 @@ $.ajax({
         console.info(data);
         $("#aaa").html(data);
     }
-});
+});*/
+
+window.onload = function () {
+    document.getElementById("username").value = "123";
+    document.forms["form_login"]["form_login_submit"].onclick = function (event) {
+        alert("event" + event);
+        // stopDefault(event);
+    }
+};
+
+function stopDefault(event) {
+    //document.getElementById("username").value = "456";
+    if (event && event.preventDefault) {//如果是FF下执行这个
+        event.preventDefault();
+    } else {
+        window.event.returnValue = false;//如果是IE下执行这个
+
+    }
+    alert("stop");
+    html_form_post();
+    return false;
+}
+
+function html_form_post() {
+    var username = document.forms["form_login"]["username"].value;
+    var password = document.forms["form_login"]["password"].value;
+    alert("username:" + document.forms["form_login"]["username"].value);
+    document.getElementById("username").value = "456";
+    document.forms["form_login"]["password"].value = "123";
+}
+
+
