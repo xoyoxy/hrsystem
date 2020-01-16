dcdd8892829dfd5bffce96ae68e9e9a21e792cd6
xoyoxy <zhoukun916@126.com>
2020-01-16 17:33:41 +0800

    htmlpost.js ajax $.post

--- src/main/java/org/renfang/html/HTMLUserController.java
+++ src/main/java/org/renfang/html/HTMLUserController.java
@@ -1,6 +1,7 @@
 package org.renfang.html;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
--- src/main/webapp/js/htmlpost.js
+++ src/main/webapp/js/htmlpost.js
@@ -13,12 +13,57 @@ $.ajax({
 });*/
 
 window.onload = function () {
-    document.getElementById("username").value = "123";
-    document.forms["form_login"]["form_login_submit"].onclick = function (event) {
-        alert("event" + event);
-        // stopDefault(event);
-    }
+    /*document.forms["form_login"].onsubmit = function (event) {
+        if(checkInput()){
+
+        }else {
+            stopDefault(event);
+        }
+
+    }*/
 };
+
+function formLoginSubmit(event, form) {
+    if(checkInput(form)){
+        var queryData={username:form["username"].value, password:form["password"].value}
+        $.post("login",JSON.stringify(queryData),function(data,status){
+            alert("数据: \n" + data + "\n状态: " + status);
+        });
+       /* $.get({
+            type : "post",
+            url : "login",
+            dataType: 'json',
+            data : JSON.stringify(queryData),
+            contentType : "application/json",
+            success : function (data) {
+// data = jQuery.parseJSON(data);
+                console.info(data);
+            }
+        });*/
+        return true;
+    }else {
+        stopDefault(event);
+        return false;
+    }
+}
+
+function checkInput(form) {
+    var warningTag = document.createElement("b");
+    warningTag.innerText = "无效，需要大于6个字符。";
+    warningTag.className = "checkInput";
+
+    var result = true;
+    if(form["username"].value.length <= 6){
+        $("#username").after(warningTag.cloneNode(true));
+        result = false;
+    }
+    if(form["password"].value.length <= 0){
+       $("#password").after(warningTag.cloneNode(true));
+        result = false;
+    }
+
+    return result;
+}
 
 function stopDefault(event) {
     //document.getElementById("username").value = "456";
@@ -28,8 +73,7 @@ function stopDefault(event) {
         window.event.returnValue = false;//如果是IE下执行这个
 
     }
-    alert("stop");
-    html_form_post();
+    alert("stop submit");
     return false;
 }
 
